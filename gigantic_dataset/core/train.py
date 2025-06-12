Added equiformer and SPE, dataset bug fixes, added load path, fixed an issue with computation of Moran's I, added testing per network, fixed plotting, some refactoring

gigantic_dataset/core/train.py
@@ -26,13 +26,15 @@ from gigantic_dataset.utils.train_utils import (
     print_single_metrics,
     wrapper_data_loader,
 )
+from gigantic_dataset.core.datasets_large import GidaSubset, get_dataset_name_from_zip_file_path
 from gigantic_dataset.utils.gen_random_mask_v8 import generate_batch_mask
 from torch_geometric.data import Data, Dataset
 from torch_geometric.loader import DataLoader
+from torch_geometric.nn import knn_graph
 import torch
 import wandb
 from gigantic_dataset.utils.train_protos import StartProfilerProto, ForwardProto, TrainOneEpochProto, TestOneEpochProto, ConfigRef
-from gigantic_dataset.utils.pe_utils import plot_pe_wandb, calculate_pe_loss, lw_tensor_local_moran, calculate_subgraph
+from gigantic_dataset.utils.pe_utils import plot_pe_wandb, calculate_auxiliary_loss, lw_tensor_local_moran, calculate_subgraph
 
 
 class WandbStartProfiler(StartProfilerProto):
@@ -83,27 +85,32 @@ class SupervisedSingleForward(ForwardProto):
 
 
 class SemiSingleForwardPE(ForwardProto):
-    def __init__(self, pe_initializer: PE_Initializer, pe_supervised: bool = False):
-        self.pe_initializer = pe_initializer
+    def __init__(self, pe_supervised: bool = False):
         self.pe_supervised = pe_supervised
 
     def __call__(
         self, models: list[Module], data: Data, batch_mask: torch.Tensor, take_first_channel: bool = True, **kwargs: Any
     ) -> tuple[Any, Any, Any | None, Any, Any, Any]:
         assert data.x is not None
-        pe_dim = models[0].get_pe_dim()
-        pe = self.pe_initializer(edge_index=data.edge_index, num_nodes=len(data.x), pe_dim=pe_dim, batch=data.batch, geo_coords=data.coordinates)
+        assert models[0].pe_initializer is not None
+        #TODO perhaps refactor this
+        pe = models[0].get_pe(data=data)
         data.y = deepcopy(data.x)
 
         data.x[batch_mask, 0] = 0
         if not self.pe_supervised:
-            print("Positional encoding is semi-supervised")
             pe[batch_mask] = 0
-        out, pe_out = models[0](x=data.x, edge_index=data.edge_index, pe=pe, batch=data.batch, edge_attr=data.edge_attr)
+        result = models[0](x=data.x, edge_index=data.edge_index, pe=pe, batch=data.batch, edge_attr=data.edge_attr)
+        if isinstance(result, tuple):
+            out, pe_out = result
+        else:
+            out = result
+            pe_out = pe
         y_pred = apply_masks(out, [batch_mask])  # out[batch_mask] #type:ignore
         y_true = data.y[batch_mask]  # type:ignore
         pe_pred = pe_out
-        pe_true = pe if not ConfigRef.config.pe_criterion == "morans-i" else lw_tensor_local_moran(y=pe, w_sparse=data.edge_index).to(pe.device)
+        pe_true = pe
+
         if not self.pe_supervised:
             pe_pred = apply_masks(pe_out, [batch_mask])
             pe_true = pe[batch_mask]
@@ -116,13 +123,17 @@ class SemiSingleForwardPE(ForwardProto):
 
 class SemiSingleForward(ForwardProto):
     def __call__(
-        self, models: list[Module], data: Data, batch_mask: torch.Tensor, take_first_channel: bool = True, **kwargs: Any
+        self, models: list[Module], data: Data, batch_mask: torch.Tensor, take_first_channel: bool = True, pass_coordinates: bool = False, **kwargs: Any
     ) -> tuple[Any, Any, Any | None]:
         assert data.x is not None
         data.y = deepcopy(data.x)
 
         data.x[batch_mask, 0] = 0
-        out = models[0](x=data.x, edge_index=data.edge_index, batch=data.batch, edge_attr=data.edge_attr)
+        if not pass_coordinates:
+            out = models[0](x=data.x, edge_index=data.edge_index, batch=data.batch, edge_attr=data.edge_attr)
+        else:
+            assert "coordinates" in data, "Coordinates are supposed to be passed, but are not included in Data object"
+            out = models[0](x=data.x, edge_index=data.edge_index, coordinates=data.coordinates, batch=data.batch, edge_attr=data.edge_attr)
 
         y_pred = apply_masks(out, [batch_mask])  # out[batch_mask] #type:ignore
         y_true = data.y[batch_mask]  # type:ignore
@@ -153,13 +164,16 @@ class TrainOneEpoch(TrainOneEpochProto):
             pt.to(device)
         len_loader_dataset = len(loader.dataset)  # type:ignore
         total_loss = 0
+        total_loss_aux = 0
         total_metric_dict = {k: 0 for k in metric_fn_dict.keys()}
         out = None
 
         for data in loader:  # Iterate in batches over the training dataset.
             [optimizer.zero_grad() for optimizer in optimizers]  # Clear gradients.
             if config.subgraphing:
-                data = calculate_subgraph(data, k=config.k, batch_size=config.batch_size_subgraph)
+                #data = calculate_subgraph(data, k=config.k, batch_size=config.batch_size_subgraph)
+                assert "coordinates" in data, "Coordinates are needed for knn-based subgraphing, but are not included in data"
+                data.edge_index = knn_graph(data.coordinates, config.k, data.batch if "batch" in data else None)
 
             num_nodes = torch.unique(data.batch, return_counts=True)[1]
             batch_mask = generate_batch_mask(num_nodes=num_nodes, edge_index=data.edge_index, mask_rate=mask_rate, required_mask=None)
@@ -179,12 +193,20 @@ class TrainOneEpoch(TrainOneEpochProto):
             # Calculate loss
             tr_loss = criterion(y_pred, y_true)
             loss = tr_loss
-            pe_loss = float('nan')
             pe_out = None
             if len(pe) == 3:
                 pe_true, pe_pred, pe_out = pe
-                pe_loss = calculate_pe_loss(pe_true, pe_pred, edge_index=data.edge_index)
-                loss = tr_loss + config.pe_loss_alpha*pe_loss
+                if ConfigRef.config.pe_criterion == "morans-i":
+                    aux_true = lw_tensor_local_moran(y=data.y, edge_index=data.edge_index, batch=data.batch).to(device)
+                    aux_true = aux_true[:, 0] # take only the pressure estimation, as in the regular criterion
+                    aux_pred = data.x[:, 0]
+                else:
+                    aux_true = pe_true
+                    aux_pred = pe_pred
+                aux_loss = calculate_auxiliary_loss(aux_true, aux_pred, edge_index=data.edge_index)
+                total_loss_aux += aux_loss
+                loss = tr_loss + config.pe_loss_alpha*aux_loss
+
             loss.backward()  # Derive gradients.
             [optimizer.step() for optimizer in optimizers]  # Update parameters based on gradients.
 
@@ -199,7 +221,7 @@ class TrainOneEpoch(TrainOneEpochProto):
         with torch.no_grad():
             dividend = max(1, len_loader_dataset)
             metric_dict = {k: total_metric_dict[k] / dividend for k in total_metric_dict.keys()}
-            return total_loss / dividend, metric_dict, out, pe_loss, pe_out
+            return total_loss / dividend, metric_dict, out, total_loss_aux / dividend, pe_out
 
 
 class TestOneEpoch(TestOneEpochProto):
@@ -209,10 +231,10 @@ class TestOneEpoch(TestOneEpochProto):
         loader: DataLoader,
         criterion: Callable[..., Any],
         metric_fn_dict: dict[str, Callable[..., Any]],
-        plot_pe: bool = False,
-        dataset_names: list[str] | None = None,
+        plot_pe: bool = False, # Plotting pe assumes that data loader only loads one type of graph topology
+        topology_name: str = "",
         **kwargs: Any,
-    ) -> tuple[float, dict, Any, float, Any | None]:
+    ) -> tuple[float, dict, Any, float]:
         config = ConfigRef.config
         func_ref = ConfigRef.ref
         device = config.device if config.device == "cuda" and torch.cuda.is_available() else "cpu"
@@ -225,6 +247,7 @@ class TestOneEpoch(TestOneEpochProto):
             pt.to(device)
         with torch.no_grad():
             total_loss = 0
+            total_loss_aux = 0
             total_metric_dict = {k: 0 for k in metric_fn_dict.keys()}
             len_loader_dataset = len(loader.dataset)  # type:ignore
             for i, data in enumerate(loader):
@@ -246,13 +269,21 @@ class TestOneEpoch(TestOneEpochProto):
                 data.coordinates = data.coordinates.to(device, non_blocking=non_blocking) if "coordinates" in data else None
 
                 y_true, y_pred, out, *pe = func_ref.forward_fn(models=models, data=data, batch_mask=batch_mask, **kwargs)
+                val_loss = criterion(y_pred, y_true)
                 pe_out = None
                 pe_loss = float('nan')
                 if len(pe) == 3:
                     pe_true, pe_pred, pe_out = pe
-                    pe_loss = criterion(pe_true, pe_pred)
+                    if ConfigRef.config.pe_criterion == "morans-i":
+                        aux_true = lw_tensor_local_moran(y=data.y, edge_index=data.edge_index, batch=data.batch).to(device)
+                        aux_true = aux_true[:, 0] # take only the pressure estimation, as in the regular criterion
+                        aux_pred = data.x[:, 0]
+                    else:
+                        aux_true = pe_true
+                        aux_pred = pe_pred
+                    aux_loss = calculate_auxiliary_loss(aux_true, aux_pred, edge_index=data.edge_index)
+                    total_loss_aux += aux_loss
 
-                val_loss = criterion(y_pred, y_true)
                 # update metrics
                 y_pred_rescaled, y_true_rescaled = func_ref.post_forward_tf_fn(y_pred, y_true)
                 total_loss += float(val_loss) * data.num_graphs
@@ -260,15 +291,22 @@ class TestOneEpoch(TestOneEpochProto):
                     computed_metric = fn(y_pred_rescaled, y_true_rescaled)
                     total_metric_dict[k] += computed_metric * data.num_graphs
                 if plot_pe and pe_out is not None:
-                    if i < len(dataset_names):
-                        plot_pe_wandb(pe=pe_out, edge_index=data.edge_index, plot_name=dataset_names[i])
-                    else:
-                        print("Error: More datasets than dataset names. Defaulting to standard dataset name.")
-                        plot_pe_wandb(pe=pe_out, edge_index=data.edge_index, plot_name=f"Default_{i}")
+                    if data.batch is None or len(data.batch.unique()) == 1:
+                        print("Only one snapshot: ", "no batch" if data.batch is None else len(data.batch.unique()))
+                        plot_pe_wandb(pe=pe_out, edge_index=data.edge_index, plot_name=topology_name)
+                        continue
+                    snapshot_ids = data.batch.unique(sorted=True)
+                    num_plots = 2
+                    chosen_snapshots = np.random.choice(snapshot_ids, size=num_plots, replace=False)
+                    pe_plot = []
+                    for id in chosen_snapshots:
+                        pe_plot.append(pe_out[data.batch == id])
+                    plot_pe_wandb(pe=pe_plot, edge_index=data.edge_index, plot_name=topology_name)
+                    
 
             dividend = max(1, len_loader_dataset)
             metric_dict = {k: total_metric_dict[k] / dividend for k in total_metric_dict.keys()}
-            return total_loss / dividend, metric_dict, out, pe_loss, pe_out
+            return total_loss / dividend, metric_dict, out, total_loss_aux / dividend
 
 
 def train(
@@ -276,7 +314,6 @@ def train(
     datasets: list[Dataset],
     train_metric_fn_dict: dict[str, Callable],
     val_metric_fn_dict: dict[str, Callable],
-    dataset_names: list[str] | None = None,
     **kwargs: Any,
 ) -> dict[str, Any]:
     sampling_strategy = kwargs.get("sampling_strategy", "batch")
@@ -316,14 +353,11 @@ def train(
             **kwargs,
         )
 
-        plot_pe = (epoch == 1 or epoch == config.epochs)
-        val_loss, val_metric_dict, _, val_loss_pe, _ = func_ref.test_one_epoch_fn(
+        val_loss, val_metric_dict, _, val_loss_pe = func_ref.test_one_epoch_fn(
             models=models,
             loader=val_loader,
             criterion=criterion,
             metric_fn_dict=val_metric_fn_dict,
-            plot_pe=plot_pe,
-            dataset_names=dataset_names if not plot_pe else [name + f"-{epoch}" for name in dataset_names],
             **kwargs,
         )
 
@@ -388,15 +422,28 @@ def eval(
     datasets: list[Dataset],
     test_metric_fn_dict: dict[str, Callable],
     plot_pe: bool = False,
-    dataset_names: list[str] | None = None,
     **kwargs: Any,
 ) -> Any | None:
     config = ConfigRef.config
     func_ref = ConfigRef.ref
 
-    test_loader = wrapper_data_loader(datasets[-1], sampling_strategy="batch", batch_size=config.batch_size, shuffle=False, pin_memory=False)
+    test_dataset = datasets[-1]
 
+    test_loader = wrapper_data_loader(test_dataset, sampling_strategy="batch", batch_size=config.batch_size, shuffle=False, pin_memory=False)
     assert isinstance(test_loader, DataLoader)
+
+    topology_loaders = []
+
+    # Create data loaders for every topology individually
+    test_ids_per_network: list[list[int]] = test_dataset.get_ids_per_network()
+    for nid, ids in enumerate(test_ids_per_network):
+        print(len(ids))
+        topology_loader = wrapper_data_loader(GidaSubset(dataset=test_dataset, indices=ids), sampling_strategy="batch", batch_size=config.batch_size, shuffle=False, pin_memory=False)
+        assert isinstance(topology_loader, DataLoader)
+        topology_loaders.append(topology_loader)
+    
+    zip_file_paths = test_dataset.zip_file_paths
+    topology_names = [get_dataset_name_from_zip_file_path(z) for z in zip_file_paths]
 
     # load reference
     criterion = func_ref.load_criterion(**kwargs)
@@ -407,15 +454,13 @@ def eval(
     dt1 = datetime.fromtimestamp(start_time)
     print("Start time:", dt1)
     print("*" * 80)
-
-    test_loss, test_metric_dict, _, pe_loss, _ = func_ref.test_one_epoch_fn(
+    
+    test_loss, test_metric_dict, _, pe_loss = func_ref.test_one_epoch_fn(
         models=models,
         loader=test_loader,
         criterion=criterion,
         metric_fn_dict=test_metric_fn_dict,
         config=config,
-        plot_pe=plot_pe,
-        dataset_names=dataset_names if not plot_pe else [name + "-test" for name in dataset_names],
         **kwargs,
     )
 
@@ -433,6 +478,26 @@ def eval(
             test_loss=test_loss,
             pe_loss=pe_loss,
             best_epoch=best_epoch,
+            test_metric_dict=test_metric_dict,
+    )
+        
+    for i, loader in enumerate(topology_loaders):
+        test_loss, test_metric_dict, _, pe_loss = func_ref.test_one_epoch_fn(
+        models=models,
+        loader=loader,
+        criterion=criterion,
+        metric_fn_dict=test_metric_fn_dict,
+        config=config,
+        plot_pe=plot_pe,
+        topology_name=topology_names[i],
+        **kwargs,
+        )
+
+        print_single_metrics(
+            epoch=0,
+            dataset_name=topology_names[i],
+            test_loss=test_loss,
+            pe_loss=pe_loss,
             test_metric_dict=test_metric_dict,
         )
 

gigantic_dataset/model/gatres.py
@@ -8,27 +8,33 @@
 
 
 from typing import Any, Callable, Literal
-
+from functools import partial
 from torch import clone, Tensor
 import torch.nn.functional as F
 import torch
 from torch.nn import Module, ModuleList
-from torch_geometric.nn import GATConv, SimpleConv, Linear, BatchNorm, GraphNorm
+from torch_geometric.nn import GATConv, SimpleConv, Linear
+from torch_geometric.data import Data
 from gigantic_dataset.utils.configs import ModelConfig
 from gigantic_dataset.utils.train_protos import ConfigRef, LoadModelProto
 from gigantic_dataset.utils.train_utils import load_weights
+from gigantic_dataset.model.gat_conv_pe import GATConvGRPE
+from gigantic_dataset.model.equiformer.graph_attention_transformer import GraphAttentionWrapper, get_standard_irreps
+from gigantic_dataset.model.equiformer.tensor_product_rescale import LinearRS
+from .pe_initializers import get_pe_initializer, PE_Initializer
+from e3nn import o3
 import os
 import torch
 
 from torch.nn import Module
 
 class GResBlockMeanConv(Module):
-    def __init__(self, in_dim, out_dim, hc, activation_func: Callable[[Tensor], Tensor] = F.relu):
+    def __init__(self, in_dim, out_dim, hc, gat_conv: Module = GATConv, activation_func: Callable[[Tensor], Tensor] = F.relu):
         super(GResBlockMeanConv, self).__init__()
 
         # self.norm1 = BatchNorm(in_channels=in_dim)
-        self.conv1 = GATConv(in_dim, hc, 2, concat=True)
-        self.conv2 = GATConv(hc * 2, out_dim, 1, concat=False)
+        self.conv1 = gat_conv(in_dim, hc, 2, concat=True)
+        self.conv2 = gat_conv(hc * 2, out_dim, 1, concat=False)
         self.mean_conv = SimpleConv(aggr="mean")
         self.activation_func = activation_func
         self.out_dim = out_dim
@@ -43,20 +49,42 @@ class GResBlockMeanConv(Module):
         x = self.activation_func(x)
         return x
 
+class EquiformerResBlockMeanConv(GResBlockMeanConv):
+    def __init__(self, in_dim, out_dim, hc, activation_func = F.relu):
+        super().__init__(in_dim, out_dim, hc, GraphAttentionWrapper, activation_func)
+    
+    def forward(self, x, edge_index, coordinates, edge_attr=None, batch: Tensor | None = None) -> Tensor:
+        x_0 = clone(x)
+        
+        x = self.activation_func(self.conv1(x, edge_index, coordinates, edge_attr, batch))
+        x = self.conv2(x, edge_index, coordinates, edge_attr, batch)
+        x = self.mean_conv(x, edge_index) + x_0
+        x = self.activation_func(x)
+        return x
 
-class GATResMeanConv(Module):
-    def __init__(self, in_dim: int, out_dim: int, name: str = "GATResMeanConv", num_blocks: int = 5, nc: int = 32, pe_dim: int = 0, concat_pe_per_layer: bool = False):
-        super(GATResMeanConv, self).__init__()
+class MeanConvBase(Module):
+    pe_initializer: PE_Initializer | None
+
+    def __init__(self, in_dim: int, out_dim: int, name: str = "EquiformerMeanConv", num_blocks: int = 5, nc: int = 8, pe_dim: int = 0):
+        super(MeanConvBase, self).__init__()
         self.in_dim = in_dim
         self.out_dim = out_dim
         self.num_blocks = num_blocks
         self.nc = nc
         self.num_blocks = num_blocks
         self.pe_dim = pe_dim
+        self.name = name
+        self.pe_initializer = get_pe_initializer(pe_dim)
+    
+    def get_pe(self, data: Data) -> Tensor | None:
+        return self.pe_initializer(data) if self.pe_initializer is not None else None
 
+
+class GATResMeanConv(MeanConvBase):
+    def __init__(self, in_dim: int, out_dim: int, name: str = "GATResMeanConv", num_blocks: int = 5, nc: int = 32, pe_dim: int = 0, concat_pe_per_layer: bool = False):
+        super(GATResMeanConv, self).__init__(in_dim, out_dim, name, num_blocks, nc, pe_dim)
         self.lin0 = Linear(in_dim, nc)
         self.blocks = ModuleList()
-        self.name = name
         if concat_pe_per_layer:
             gat_out_dim = nc
         else: 
@@ -80,15 +108,14 @@ class GATResMeanConv(Module):
     
 class GATResMeanConvLSPE(GATResMeanConv):
     def __init__(self, in_dim: int, out_dim: int, name: str = "GATResMeanConvLSPE", num_blocks: int = 5, nc: int = 32, pe_dim: int = 20):
-        #TODO determine the correct dimension for the positional encoding
         super(GATResMeanConvLSPE, self).__init__(in_dim=in_dim, out_dim=out_dim, name=name, num_blocks=num_blocks, nc=nc, pe_dim=pe_dim, concat_pe_per_layer=True)
-        self.lin0_pe = Linear(self.pe_dim, self.pe_dim)
+        #self.lin0_pe = Linear(self.pe_dim, self.pe_dim)
         self.blocks_pe = ModuleList()
         for _ in range(self.num_blocks):
             # The positional encoding uses tanh and not relu to allow negative coordinates
             block = GResBlockMeanConv(in_dim=self.pe_dim, out_dim=self.pe_dim, hc=self.pe_dim, activation_func=torch.tanh)
             self.blocks_pe.append(block)
-        self.lin1_pe = Linear(self.pe_dim, self.pe_dim)
+        #self.lin1_pe = Linear(self.pe_dim, self.pe_dim)
 
     def forward(self, x: Tensor, edge_index: Tensor, pe: Tensor, batch: Tensor | None = None, edge_attr: Tensor | None = None) -> tuple[Tensor, Tensor]:
         x = self.lin0(x)
@@ -104,17 +131,41 @@ class GATResMeanConvLSPE(GATResMeanConv):
         #pe = self.lin1_pe(pe)
         return x, pe
 
-class PE_GATResMeanConv(GATResMeanConv):
+class PE_concat_GATResMeanConv(GATResMeanConv):
+    def __init__(self, in_dim, out_dim, name = "GATResMeanConv", num_blocks = 5, nc = 32, pe_dim = 0, concat_pe_per_layer = False):
+        super(PE_concat_GATResMeanConv, self).__init__(in_dim, out_dim, name, num_blocks, nc, pe_dim, concat_pe_per_layer)
+
     def forward(self, x: Tensor, edge_index: Tensor, pe: Tensor, batch: Tensor | None = None, edge_attr: Tensor | None = None) -> Tensor:
         x = self.lin0(x)
         x = torch.cat((x,pe), -1)
         for i in range(self.num_blocks):
             x = self.blocks[i](x, edge_index, edge_attr, batch)
         x = self.lin1(x)
-        return x
+        return x, pe
     
 
+class EquiformerMeanConv(MeanConvBase):
+    def __init__(self, in_dim: int, out_dim: int, name: str = "EquiformerMeanConv", num_blocks: int = 5, nc: int = 8, concat_pe_per_layer: bool = False, *args, **kwargs):
+        super(EquiformerMeanConv, self).__init__(in_dim, out_dim, name, num_blocks, nc, 0)
+        self.input_irreps = o3.Irreps('{}x0e'.format(self.in_dim))
+
+        self.hidden_irreps = get_standard_irreps(self.nc)
+
+        self.lin0 = LinearRS(self.input_irreps, self.hidden_irreps)
+        self.blocks = ModuleList()
+        for _ in range(self.num_blocks):
+            block = EquiformerResBlockMeanConv(in_dim=nc, out_dim=nc, hc=nc, activation_func=partial(F.leaky_relu, negative_slope=0.2))
+            self.blocks.append(block)
+
+        self.lin1 = LinearRS(self.hidden_irreps, o3.Irreps('{}x0e'.format(self.out_dim)))
     
+    def forward(self, x: Tensor, edge_index: Tensor, coordinates: Tensor, batch: Tensor | None = None, edge_attr: Tensor | None = None):
+        x = self.lin0(x)
+        for i in range(self.num_blocks):
+            x = self.blocks[i](x, edge_index, coordinates, edge_attr, batch)
+        x = self.lin1(x)
+        return x
+
 class LoadModel(LoadModelProto):
     def __call__(
         self,
@@ -144,16 +195,16 @@ class LoadModel(LoadModelProto):
                 models, _ = load_weights(path=model_config.weight_path, models=[model], load_keys=[model_config.name])
                 model = models[0]
             modules.append(model)
-        if load_weights_from == "train_config" and train_config.save_path != "" and os.path.exists(train_config.save_path):
+        if load_weights_from == "train_config" and train_config.load_path != "" and os.path.exists(train_config.load_path):
             filter_word = "best" if do_load_best else "last"
-            model_weight_paths = [entry for entry in os.listdir(train_config.save_path) if "training_log" not in entry and filter_word in entry]
+            model_weight_paths = [entry for entry in os.listdir(train_config.load_path) if "training_log" not in entry and filter_word in entry]
             if len(model_weight_paths) > 0:
                 assert len(model_weight_paths) == len(modules)
                 for i in range(len(modules)):
                     matching_order_paths = [path for path in model_weight_paths if str(i) in path]
                     matching_order_path = matching_order_paths[0]
                     tmps, _ = load_weights(
-                        path=os.path.join(train_config.save_path, matching_order_path), models=[modules[i]], load_keys=[model_configs[i].name]
+                        path=os.path.join(train_config.load_path, matching_order_path), models=[modules[i]], load_keys=[model_configs[i].name]
                     )
                     modules[i] = tmps[0].to(train_config.device)
 

gigantic_dataset/model/pe_initializers.py
@@ -1,29 +1,44 @@
+from typing import Callable, Any
 import torch
 from torch import Tensor, nn
-from torch_geometric.utils import degree, subgraph
+import torch.nn.functional as F
+from torch_geometric.utils import degree, subgraph, unbatch, get_laplacian
+from torch_geometric.data import Data
 from abc import ABC, abstractmethod
+from gigantic_dataset.utils.train_protos import ConfigRef
 from gigantic_dataset.utils.pe_utils import sparse_diagonal, GridCellSpatialRelationEncoder
+from gigantic_dataset.model.SPE.gin import GINRho
+from gigantic_dataset.model.SPE.mlp import MLP
 import numpy as np
+from scipy.sparse import coo_matrix
+from scipy.sparse.linalg import eigsh
 
-class PE_Initializer(ABC):
+# This is a module so I can register it in the main model and load the initializer together with the model
+class PE_Initializer(nn.Module):
+    pe_dim: int
+    def __init__(self, pe_dim: int = 20):
+        super(PE_Initializer, self).__init__()
+        self.pe_dim = pe_dim
+
     @abstractmethod
-    def __call__(self, num_nodes: int, pe_dim: int, edge_index: Tensor | None = None, batch: Tensor | None = None, geo_coords: Tensor | None = None) -> Tensor:
+    def forward(self, data: Data) -> Tensor:
         pass
 
 class RWPE_Initializer(PE_Initializer):
-    def __call__(self, num_nodes: int, pe_dim: int, edge_index: Tensor | None = None, batch: Tensor | None = None, geo_coords: Tensor | None = None) -> Tensor:
-        assert edge_index is not None, "Trying to initialize random walk positional encoding, but edge indices are None"
-        if batch is None or len(batch.unique()) == 1:
-            return self.calculate_rwpe(edge_index=edge_index, num_nodes=num_nodes, pe_dim=pe_dim)
+    def forward(self, data: Data) -> Tensor:
+        assert data.edge_index is not None, "Trying to initialize random walk positional encoding, but edge indices are None"
+        num_nodes = len(data.x)
+        if not "batch" in data or len(data.batch.unique()) == 1:
+            return self.calculate_rwpe(edge_index=data.edge_index, num_nodes=num_nodes, pe_dim=self.pe_dim)
         # if there are multiple graphs in the data, call the function per data, store the node order, and rearrange the pe to be in the correct position
-        pe = torch.zeros((num_nodes, self.pe_dim), device=edge_index.device)
-        for graph_id in batch.unique(sorted=True):
+        pe = torch.zeros((num_nodes, self.pe_dim), device=data.edge_index.device)
+        for graph_id in data.batch.unique(sorted=True):
             # Get the indices of the nodes in this subgraph
-            node_mask = (batch == graph_id)
+            node_mask = (data.batch == graph_id)
             node_indices = node_mask.nonzero()[0]
             # Generate a subgraph, calculate its rwpe, and store it at the correct indices
-            subgraph_edge_index, _ = subgraph(node_indices, edge_index, relabel_nodes=True, num_nodes=num_nodes)
-            sub_pe = self.calculate_rwpe(subgraph_edge_index, len(node_indices), pe_dim)
+            subgraph_edge_index, _ = subgraph(node_indices, data.edge_index, relabel_nodes=True, num_nodes=num_nodes)
+            sub_pe = self.calculate_rwpe(subgraph_edge_index, len(node_indices), self.pe_dim)
             pe[node_indices] = sub_pe
         return pe        
     
@@ -48,6 +63,8 @@ class RWPE_Initializer(PE_Initializer):
 # This class is adopted from the initialization of the PE-GCN example in https://github.com/konstantinklemmer/pe-gnn.  
 class GeoPE_Initializer(PE_Initializer):
     def __init__(self, coord_dim: int = 2, pe_hidden_dim=128, pe_dim: int = 20):
+        super(GeoPE_Initializer, self).__init__(pe_dim)
+        self.pe_dim = pe_dim
         self.coord_dim = coord_dim
         self.emb_hidden_dim = pe_hidden_dim
         self.emb_dim = pe_dim
@@ -61,9 +78,112 @@ class GeoPE_Initializer(PE_Initializer):
             nn.Linear(pe_hidden_dim // 4, pe_dim)
         )
         self.decoder.to(self.spatial_encoder.device)
-    def __call__(self, num_nodes: int, pe_dim: int, geo_coords: Tensor | None = None, **kwargs) -> Tensor:
-        assert geo_coords is not None, "Trying to initialize geo-located positional encoding, but coordinates are None"
-        geo_coords = geo_coords.reshape(1, geo_coords.shape[0], geo_coords.shape[1])
+    def forward(self, data: Data) -> Tensor:
+        assert "coordinates" in data, "Trying to initialize geo-located positional encoding, but coordinates are not in Data object"
+        geo_coords = data.coordinates.reshape(1, data.coordinates.shape[0], data.coordinates.shape[1])
         emb = self.spatial_encoder(geo_coords.detach().cpu().numpy())
         emb = emb.reshape(emb.shape[1], emb.shape[2])
         return self.decoder(emb).float()
+    
+class SPE_Initializer(PE_Initializer):
+    rho: nn.Module
+    phi_list: nn.ModuleList
+    '''
+    With pe_hidden_dim at 128 and 64 it runs out of cuda memory, hence it is reduced to 32
+    '''
+    def __init__(self, pe_dim: int = 20, pe_hidden_dim: int = 32) -> None:
+        super(SPE_Initializer, self).__init__(pe_dim)
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.eig_amt = pe_dim
+        m = 8
+        self.rho = GINRho(n_layers=3, in_dims=m, hidden_dims=pe_hidden_dim, out_dims=self.eig_amt, bn=False).to(self.device) # These values are adopted from the config in the SPE codebase, changed n_layers from 8 to 3
+        self.phi_list = nn.ModuleList()
+        for i in range(m):
+            self.phi_list.append(MLP(1, 1, hidden_dims=16, n_layers=3).to(self.device))
+
+        self.linear = nn.Linear(self.eig_amt, pe_dim).to(self.device)
+
+    def calculate_eigs(self, edge_index: Tensor, num_nodes: int) -> tuple[Any, Any]:
+        lap_edge_index, lap_edge_weight = get_laplacian(edge_index=edge_index, normalization="sym") # Get the normalized laplacian
+        laplacian = coo_matrix((lap_edge_weight.cpu(), (lap_edge_index[0].cpu().numpy(), lap_edge_index[1].cpu().numpy())), shape=(num_nodes, num_nodes))
+        k = min(num_nodes, self.eig_amt)
+        return eigsh(laplacian, k=k, which="SM") 
+
+    def forward(self, data: Data) -> torch.Tensor:
+        """
+        :param data: Data object containing edge_index [2, E_sum] and batch [N_sum] Tensors
+        :param batch: Batch index vector. [N_sum]
+        :return: Positional encoding matrix. [N_sum, D_pe]
+        """
+        device = data.x.device
+
+        Lambda = []
+        V = []
+        if data.batch == None:
+            num_nodes = len(data.x)
+            eigenvalues, V = self.calculate_eigs(data.edge_index, num_nodes)
+            Lambda = [eigenvalues]
+            V = np.array([V])
+            data.batch = torch.zeros(num_nodes, dtype=torch.int64, device=device)
+        else:
+            for graph_id in data.edge_index.unique(sorted=True):
+                node_mask = (data.batch == graph_id)
+                node_indices = node_mask.nonzero()[0]
+                num_nodes = node_mask.sum()
+                # Generate a subgraph, calculate its eigenvalues and eigenvectors
+                subgraph_edge_index, _ = subgraph(node_indices, data.edge_index, relabel_nodes=True, num_nodes=num_nodes)
+                
+                eigenvalues, eigenvectors = self.calculate_eigs(subgraph_edge_index, num_nodes)
+                Lambda.append(eigenvalues)
+                V.append(eigenvectors)
+        
+        Lambda = torch.tensor(np.array(Lambda), device=device)
+        if Lambda.shape[1] < self.eig_amt:
+            F.pad(Lambda, (0, self.eig_amt - Lambda.shape[1]), value=0.0)
+        
+        # Mask invalid eigenvalues
+        a = torch.arange(0, Lambda.size(1)).unsqueeze(0).to(Lambda.device)
+        mask = torch.cat([a < torch.sum(data.batch == i) for i in range(data.batch[-1]+1)], dim=0) # [B, D_pe, 1]
+        Lambda = Lambda * mask
+        del mask
+        Lambda = Lambda.unsqueeze(dim=2)   # [B, eig_amt, 1]
+
+        Z = torch.stack([
+            phi(Lambda).squeeze(dim=2)     # [B, eig_amt]
+            for phi in self.phi_list
+        ], dim=2)                          # [B, eig_amt, M]
+
+        V_list = torch.tensor(np.array(V), device=device) # [N_i, eig_amt] * B
+        if V_list.shape[2] < self.eig_amt:
+            F.pad(Lambda, (0, self.eig_amt - V_list.shape[2]), value=0.0)
+        Z_list = list(Z)                    # [eig_amt, M] * B
+
+        W_list = []                        # [N_i, N_i, M] * B
+        for V, Z in zip(V_list, Z_list):   # [N_i, eig_amt] and [eig_amt, M]
+            V = V.unsqueeze(dim=0)         # [1, N_i, eig_amt]
+            Z = Z.permute(1, 0)            # [M, eig_amt]
+            Z = Z.diag_embed()             # [M, eig_amt, eig_amt]
+            V_T = V.mT                     # [1, eig_amt, N_i]
+            W = V.matmul(Z).matmul(V_T)    # [M, N_i, N_i]
+            # W = V.matmul(V_T).repeat([Z.size(0), 1, 1])
+            W = W.permute(1, 2, 0)         # [N_i, N_i, M]
+            W_list.append(W)
+        rho = self.rho(W_list, data.edge_index) # [N_sum, eig_amt]
+        # I have had issues with running out of memory, so I clean up after the initialization is complete
+        del W_list, V_list, Z_list, V, Z, W
+        torch.cuda.empty_cache()
+        return self.linear(rho) # [N_sum, D_pe]
+
+    @property
+    def out_dims(self) -> int:
+        return self.rho.out_dims
+    
+def get_pe_initializer(pe_dim: int = 20) -> PE_Initializer | None:
+    pe_init = ConfigRef.config.pe_init
+    if pe_init == "rw":
+            return RWPE_Initializer(pe_dim=pe_dim)
+    elif pe_init == "geo":
+        return GeoPE_Initializer(pe_dim=pe_dim)
+    elif pe_init == "spe":
+        return SPE_Initializer(pe_dim=pe_dim)
+    return None

gigantic_dataset/utils/pe_utils.py
@@ -1,3 +1,4 @@
+from typing import Callable, List
 import sklearn
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
@@ -7,6 +8,7 @@ import torch.nn.functional as F
 from torch_geometric.utils import get_laplacian
 from torch_geometric.data import Data
 from torch_geometric.nn import knn_graph
+from torch_scatter import scatter_mean, scatter_std, scatter_min, scatter_max, scatter_add
 import wandb
 import numpy as np
 import math
@@ -14,34 +16,47 @@ from collections import defaultdict
 from typing import Any
 from gigantic_dataset.utils.train_protos import ConfigRef
 
-def plot_pe_wandb(pe: Tensor, edge_index: Tensor, plot_name: str, plot_edges: bool = False) -> None:
-    # Verify that pe has admissible values
-    if pe.std() == 0:
-        print("Warning: Positional encoding has zero standard deviation. Skipping PE visualization.")
-        return
-    if torch.isnan(pe).any():
-        print("Warning: Positional encoding contains NaNs. They are converted to zeros.")
-    #TODO fix this
-    return 
-    pe = pe.cpu().numpy()
-    tsne = sklearn.manifold.TSNE(n_components=2, random_state=42) #TODO perhaps change perplexity and random_state parameter
-    z_2d = tsne.fit_transform(pe)
-    fig, ax = plt.subplots(figsize=(16,12))
-    
-    ax.scatter(z_2d[:, 0], z_2d[:, 1], s=10, color='darkblue')
-    ax.set_title("t-SNE visualization of positional encoding")
-    ax.set_xlabel("t-SNE dim 1")
-    ax.set_ylabel("t-SNE dim 2")
+def plot_pe_wandb(pe: Tensor | list[Tensor], edge_index: Tensor, plot_name: str, plot_edges: bool = False) -> None:
+    if isinstance(pe, Tensor):
+        pe = [pe]
+    print("Number of snapshots: ", len(pe))
+    if len(pe) > 2:
+        print("Warning, more than two positional encodings to be visualized.", pe)
+        fig, axs = plt.subplots(1, 2, figsize=(2*8,6))
+    else:
+        fig, axs = plt.subplots(1, len(pe), figsize=(len(pe)*8,6))
 
-    if plot_edges:
-        segments = [
-            [z_2d[edge_index[0][i]], z_2d[edge_index[1][i]]]
-            for i in range(edge_index.shape[1])
-        ]
-        lc = LineCollection(segments, linewidths=0.2, alpha=0.5, color='blue')
-        ax.add_collection(lc)
+    for i, pe_snapshot in enumerate(pe):
+        if i >= 2:
+            break
+        ax = axs[i] if len(pe) > 1 else axs
+        if pe_snapshot.std() == 0:
+            print("Warning: Positional encoding has zero standard deviation. Adding a small amount of noise.")
+            pe_snapshot += 1e-4 * torch.randn_like(pe_snapshot)
+        if torch.isnan(pe_snapshot).any():
+            print("Warning: Positional encoding contains NaNs. They are converted to zeros.")
+            pe_snapshot = torch.nan_to_num(pe_snapshot)
+
+        pe_snapshot = pe_snapshot.cpu().numpy()
+        perplexity = 30.0 if len(pe_snapshot) > 30.0 else len(pe_snapshot) - 1.0
+        tsne = sklearn.manifold.TSNE(n_components=2, random_state=42, perplexity=perplexity) #TODO perhaps change perplexity and random_state parameter
+        z_2d = tsne.fit_transform(pe_snapshot)
+        
+        ax.scatter(z_2d[:, 0], z_2d[:, 1], s=10, color='darkblue')
+        ax.set_title("t-SNE visualization of positional encoding")
+        ax.set_xlabel("t-SNE dim 1")
+        ax.set_ylabel("t-SNE dim 2")
+
+        if plot_edges:
+            segments = [
+                [z_2d[edge_index[0][i]], z_2d[edge_index[1][i]]]
+                for i in range(edge_index.shape[1])
+            ]
+            lc = LineCollection(segments, linewidths=0.2, alpha=0.5, color='blue')
+            ax.add_collection(lc)
 
     wandb.log({plot_name: wandb.Image(fig)})
+    plt.close(fig)
 
 def sparse_diagonal(G: Tensor):
         G = G.coalesce()
@@ -72,10 +87,10 @@ def laplacian_eigenvector_loss(pe: Tensor, laplacian: Tensor, lambda_ortho: int 
     ortho = F.mse_loss(pe_t @ pe, identity_matrix) # Since pe is normalized, frobenius norm is equal to k * mse, and the k cancels out
     return trace + lambda_ortho * ortho
 
-def calculate_pe_loss(pe: Tensor, pe_out: Tensor, edge_index: Tensor | None = None, edge_weight: Tensor | None = None):
+def calculate_auxiliary_loss(pe: Tensor, pe_out: Tensor, edge_index: Tensor | None = None, edge_weight: Tensor | None = None):
     loss_criterion = ConfigRef.config.pe_criterion
-    if loss_criterion == "mse" or loss_criterion == "morans_i":
-        assert pe.shape == pe_out.shape
+    if loss_criterion == "mse" or loss_criterion == "morans-i":
+        assert pe.shape == pe_out.shape, f"Cannot compute MSE: Shape of target is {pe.shape}, but shape of output is {pe_out.shape}"
         return torch.nn.MSELoss(reduction="mean").to(ConfigRef.config.device)(pe_out, pe)
     elif loss_criterion == "laplacian":
         assert edge_index is not None, f"Edge indices are required for calculation of {loss_criterion} loss, but edge_index is None."
@@ -140,33 +155,38 @@ def get_activation_function(activation, context_str):
     else:
         raise Exception("{} activation not recognized.".format(context_str))
     
-def normal_torch(tensor,min_val=0):
-    t_min = torch.min(tensor)
-    t_max = torch.max(tensor)
-    if t_min == 0 and t_max == 0:
-        return torch.tensor(tensor)
+def normal_torch(tensor, batch, min_val=0):
+    t_min = scatter_min(tensor, batch, dim=0)[0]
+    t_max = scatter_max(tensor, batch, dim=0)[0]
+
+    den = t_max - t_min
+    den[den == 0] = 1
+
     if min_val == -1:
-        tensor_norm = 2 * ((tensor - t_min) / (t_max - t_min)) - 1
+        tensor_norm = 2 * ((tensor - t_min[batch]) / den[batch]) - 1
     if min_val== 0:
-        tensor_norm = ((tensor - t_min) / (t_max - t_min))
-    return torch.tensor(tensor_norm)
+        tensor_norm = ((tensor - t_min[batch]) / den[batch])
+    return tensor_norm
     
-def lw_tensor_local_moran(y,w_sparse,na_to_zero=True,norm=True,norm_min_val=0):
+def lw_tensor_local_moran(y, edge_index, batch=None, na_to_zero=True, norm=True, norm_min_val=0):
     device = ConfigRef.config.device
-    y = y.reshape(-1)
-    n = len(y)
-    n_1 = n - 1
-    z = y - y.mean()
-    sy = y.std()
-    z /= sy
-    den = (z * z).sum()
-    zl = torch.tensor(w_sparse * z).to(device)
-    mi = n_1 * z * zl / den
+    if batch == None:
+        batch = torch.zeros(len(y)).to(device).long()
+    n_1_per_graph = torch.bincount(batch) - 1
+    y_mean_per_graph = scatter_mean(y, batch, dim=0)
+    y_sd_per_graph = scatter_std(y, batch, dim=0)
+    y_sd_per_graph[y_sd_per_graph==0] = 1
+    z = (y - y_mean_per_graph[batch]) / y_sd_per_graph[batch]
+    den = scatter_add(z * z, batch, dim=0)
+    w_sparse = torch.sparse_coo_tensor(edge_index, torch.ones(edge_index.shape[1], device=device), size=(len(y), len(y)), device=device).coalesce()
+    zl = torch.sparse.mm(w_sparse, z).to(device)
+    mi = n_1_per_graph[batch].unsqueeze(1) * z * zl / den[batch]
     if na_to_zero==True:
-        mi[torch.isnan(mi)] = 0
+        mi = torch.nan_to_num(mi, nan=0.0)
     if norm==True:
-        mi = normal_torch(mi,min_val=norm_min_val)
-    return torch.tensor(mi)
+        mi = normal_torch(mi, batch, min_val=norm_min_val)
+    print("moran's I shape: ", mi.shape)
+    return torch.tensor(mi) # This is intentional
 
 # This class is copied from the original PE-GNN implementation. See https://github.com/konstantinklemmer/pe-gnn
 class SingleFeedForwardNN(nn.Module):

gigantic_dataset/utils/train_protos.py
@@ -349,7 +349,7 @@ def pre_proccessing(
             assert tf is not None
             my_tfs.append(tf)
 
-        if len(my_tfs) > 2:
+        if len(my_tfs) > 1:
             composer = Compose(my_tfs)
             train_set.transform = composer
         else:
@@ -367,16 +367,17 @@ def load_gida_datasets(
     gida_config: GidaConfig,
     custom_stats_tuple_pt_path: str = "",
     custom_subset_shuffle_pt_path: str = "",
+    is_training: bool = True
 ) -> list[Dataset]:
     """Function supports loading gida datasets with GiDa Interface.<br />
-    NOTE: If you want pre-define stats tuple when computing normalization, override `custom_stats_tuple_pt_path` with the dataset_log.pt of elsewhere training config. If not, we reuse the `dataset_log.pt` from `save_path` of the CURRENT Training Config. <br />
+    NOTE: If you want pre-define stats tuple when computing normalization, override `custom_stats_tuple_pt_path` with the dataset_log.pt of elsewhere training config. If not, we reuse the `dataset_log.pt` from `load_path` of the CURRENT Training Config. <br />
     NOTE: If testing on different network and ensuring on the same subset of test set, user should override `custom_subset_shuffle_pt_path` in `gida_config`. <br />
-    By default, we use `stat_tuple`, which is stored in `dataset_log.pt` in the folder `save_path` of training config accessed via a singleton interface, to do normalization.<br />
+    By default, we use `stat_tuple`, which is stored in `dataset_log.pt` in the folder `load_path` of training config accessed via a singleton interface, to do normalization.<br />
     NOTE: pre_processing only normalize 'x' features<br />
     Args:
         gida_config (GidaConfig): gida confi
-        custom_stats_tuple_pt_path (str, optional): For loading stat tuple, this parameter is for external loading statistic tuple from elsewhere. In such a case, add the path of `dataset_log.pt` of the testing network here. If `custom_stats_tuple_path==""`, we reuse the `dataset_log.pt` from `save_path` of Trainning Config. Defaults to "".
-        custom_subset_shuffle_pt_path (str, optional): For loading subset shuffle ids, this parameter is for external loading statistic tuple from elsewhere. In such a case, add the path of `dataset_log.pt` of the testing network here. If `custom_subset_shuffle_path==""`, we reuse the `dataset_log.pt` from `save_path` of Trainning Config. Defaults to "".
+        custom_stats_tuple_pt_path (str, optional): For loading stat tuple, this parameter is for external loading statistic tuple from elsewhere. In such a case, add the path of `dataset_log.pt` of the testing network here. If `custom_stats_tuple_path==""`, we reuse the `dataset_log.pt` from `load_path` of Trainning Config. Defaults to "".
+        custom_subset_shuffle_pt_path (str, optional): For loading subset shuffle ids, this parameter is for external loading statistic tuple from elsewhere. In such a case, add the path of `dataset_log.pt` of the testing network here. If `custom_subset_shuffle_path==""`, we reuse the `dataset_log.pt` from `load_path` of Trainning Config. Defaults to "".
 
     Returns:
         list[Dataset]: _description_
@@ -384,15 +385,19 @@ def load_gida_datasets(
     train_set, valid_set, test_set = None, None, None
 
     # default path (if empty, we create)
-    defaut_dataset_log_pt_path = osp.join(ConfigRef.config.save_path, "gida_dataset_log.pt")
-    gida_config.dataset_log_pt_path = defaut_dataset_log_pt_path
+    if is_training:
+        defaut_dataset_log_pt_path = osp.join(ConfigRef.config.load_path, "gida_dataset_log.pt")
+        gida_config.dataset_log_pt_path = defaut_dataset_log_pt_path
 
     # call interface and implicitly load subset shuffle ids from the custom path
     gida_param_dict = gida_config.as_dict()
     full_gida = GidaV6(**gida_param_dict)
 
     # we perform subset_shuffle prior the custom file. If it is empty, we try to load from dataset_log.pt. If it is failed, we create some and save to dataset_log.pt
-    full_gida.process_subset_shuffle(custom_subset_shuffle_pt_path=custom_subset_shuffle_pt_path, create_and_save_to_dataset_log_if_nonexist=True)
+    #full_gida.process_subset_shuffle(custom_subset_shuffle_pt_path=custom_subset_shuffle_pt_path, create_and_save_to_dataset_log_if_nonexist=True)
+    full_gida.process_subset_shuffle_custom(custom_subset_shuffle_pt_path=custom_subset_shuffle_pt_path,
+                                            sampling_strategy=gida_config.sampling_strategy,
+                                            create_and_save_to_dataset_log_if_nonexist=True)
 
     train_set, valid_set, test_set = pre_proccessing(
         full_gida=full_gida,
@@ -412,7 +417,7 @@ def load_gida_datasets(
 
 def default_load_optimizers(
     models: list[nn.Module],
-    load_from: Literal["best_in_save_path", "last_in_save_path", "per_model_weight_path"] = "best_in_save_path",
+    load_from: Literal["best_in_load_path", "last_in_load_path", "per_model_weight_path"] = "best_in_load_path",
     is_shared_optim: bool = False,
     **kwds: Any,
 ) -> list[Optimizer]:
@@ -427,9 +432,9 @@ def default_load_optimizers(
             optimizers = [Adam(model.parameters(), lr=config.lr, weight_decay=config.weight_decay) for model in models]
 
     try:
-        if load_from in ["best_in_save_path", "last_in_save_path"] and config.save_path != "":
+        if load_from in ["best_in_load_path", "last_in_load_path"] and config.load_path != "":
             prefix = load_from.split("_")[0]
-            latest_file = osp.join(config.save_path, prefix + "_training_log.pt")
+            latest_file = osp.join(config.load_path, prefix + "_training_log.pt")
             cp_dict = load(latest_file)
             optimizers_state_dict = cp_dict["optimizers_state_dict"]
             for i, optim_state in optimizers_state_dict.items():